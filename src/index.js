338ca5605a4b74c9524911abdf24a2979d7b8754
Swastik Kakran <kakranswastik2007@gmail.com>
2026-02-13 12:10:41 +0530

    create an express server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"./.env"
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("App is listening...")
+})
