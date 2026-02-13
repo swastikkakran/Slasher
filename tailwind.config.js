50efcba203ae1fc382d2468971542b28e50e43e9
Swastik Kakran <kakranswastik2007@gmail.com>
2026-02-13 15:07:53 +0530

    add landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ export default {
     "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'golden-dust': '#D4AF37',
+        'mars-brown': '#8B4513',
+        'golden-light': '#F4E4C1',
+        'mars-dark': '#5C2E0A'
+  }
+    },
   },
   plugins: [],
 }
